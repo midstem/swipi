41e17e40932c2ec102d769c2cd480782ebfd3edd
alex-dishen <102871422+alex-dishen@users.noreply.github.com>
2023-01-29 05:36:13 +0200

    Prepare src folder to the publishing

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { Slider } from './Slider';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default Slider;
