58e9ebd7f1457d875a9e7d3c2050fc617418b1eb
alex-dishen <102871422+alex-dishen@users.noreply.github.com>
2023-02-15 21:10:27 +0200

    Change cursor according to actions

--- src/ui/SlidesContainer.tsx
+++ src/ui/SlidesContainer.tsx
@@ -1,30 +1,39 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
 import { SlidesContainerProps } from './types';
 
 const SlidesContainer = ({
   children,
   transform,
   animation,
-}: SlidesContainerProps) => (
-  <div
-    // onDragStart={(e) => {
-    //   e.preventDefault();
-    // }}
-    style={{
-      display: 'flex',
-      width: 'fit-content',
-      transform: `translate3d(${transform}px, 0, 0)`,
-      transition: `${animation ? `all 0.3s ease-out 0s` : `0s`}`,
-      height: '100%',
-      //   &:hover {
-      //     cursor: grab;
-      //   }
-      //   &:active {
-      //     cursor: grabbing;
-    }}
-  >
-    {children}
-  </div>
-);
+}: SlidesContainerProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleHovered = () => setIsHovered(!isHovered);
+  const toggleActive = () => setIsActive(!isActive);
+
+  return (
+    <div
+      onDragStart={(e) => {
+        e.preventDefault();
+      }}
+      onMouseEnter={toggleHovered}
+      onMouseDown={toggleActive}
+      onMouseLeave={toggleHovered}
+      onMouseUp={toggleActive}
+      style={{
+        display: 'flex',
+        width: 'fit-content',
+        transform: `translate3d(${transform}px, 0, 0)`,
+        transition: `${animation ? `all 0.3s ease-out 0s` : `0s`}`,
+        height: '100%',
+        cursor: isHovered && isActive ? 'grabbing' : 'grab',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default SlidesContainer;
