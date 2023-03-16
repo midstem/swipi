421f9582428fa42761f60655a3c959235e116e78
Oleksandr <didyshen.oleksandr@gmail.com>
2023-03-16 12:34:01 +0200

    Add classes for buttons

--- src/UI/SliderButton.tsx
+++ src/UI/SliderButton.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable react/display-name */
 import React, { forwardRef, RefObject } from 'react';
 import { SliderButtonProps } from 'UI/types';
 
 const SliderButton = forwardRef<HTMLButtonElement, SliderButtonProps>(
   ({ children, onClick }, ref): JSX.Element => (
     <button
+      className={ref ? 'right-button' : 'left-button'}
       ref={ref as RefObject<HTMLButtonElement>}
       onClick={onClick}
       type="button"
