809236d8dfc790d53409b91fa8aeca146439a373
Oleksandr Didyshen <102871422+alex-dishen@users.noreply.github.com>
2023-05-10 08:06:01 +0300

    Update project build (#104)

--- src/UI/DotsWrapper.tsx
+++ src/UI/DotsWrapper.tsx
@@ -3,4 +3,5 @@ import { ComponentBasicProps } from './types'
 const DotsWrapper = ({ children }: ComponentBasicProps): JSX.Element => (
   <div className="dots-wrapper">{children}</div>
 )
+
 export default DotsWrapper
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import Slider from './Slider'
-import { ConfigType } from './Slider/types'
 
-export { ConfigType }
 export default Slider
