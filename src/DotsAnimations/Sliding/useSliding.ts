42ff3f6a11c3e986448b16d9e03ad513b9463ad4
Oleksandr Didyshen <102871422+alex-dishen@users.noreply.github.com>
2023-05-24 08:38:44 +0300

    Eliminate app crushing when changing dots animation (#121)

--- src/DotsAnimations/Sliding/useSliding.ts
+++ src/DotsAnimations/Sliding/useSliding.ts
@@ -6,27 +6,26 @@ import { getDotsLeftOffsets, getWidthDifference } from './helpers'
 const useSliding = (slideIndex: number) => {
   const dotsRef = useRef<(HTMLDivElement | null)[]>([])
   const activeDotRef = useRef<HTMLDivElement>(null)
+  const dotsLeftOffsetsRef = useRef<DotsLeftOffsetsTypes[]>(defaultDotsLeftOffsets)
   const [activeDotLeft, setActiveDotLeft] = useState<number>(0)
   const [dotWidth, setDotWidth] = useState<number>(0)
   const [activeDotWidth, setActiveDotWidth] = useState<number>(0)
   //prettier-ignore
-  const [dotsLeftOffsets, setDotsLeftOffsets] = 
-  useState<DotsLeftOffsetsTypes[]>(defaultDotsLeftOffsets)
 
   const initializeData = (): void => {
-    setDotsLeftOffsets(getDotsLeftOffsets(dotsRef))
+    dotsLeftOffsetsRef.current = getDotsLeftOffsets(dotsRef)
     setDotWidth(dotsRef.current[0]?.clientWidth ?? 0)
     setActiveDotWidth(activeDotRef.current?.clientWidth ?? 0)
   }
 
   const moveActiveDot = useCallback(() => {
-    const activeDotIndent = dotsLeftOffsets[slideIndex].left
+    const activeDotIndent = dotsLeftOffsetsRef.current[slideIndex].left
     const dotAlignment = getWidthDifference(dotWidth, activeDotWidth)
 
     if (!slideIndex) return setActiveDotLeft(activeDotIndent + dotAlignment)
 
     setActiveDotLeft(activeDotIndent + dotAlignment)
-  }, [activeDotWidth, dotWidth, dotsLeftOffsets, slideIndex])
+  }, [activeDotWidth, dotWidth, slideIndex])
 
   useEffect(() => {
     initializeData()
