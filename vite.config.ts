f003458a930cd4db500192add844569b3a3a6e8d
Oleksii Latyshev <latyshevskiymail@gmail.com>
2026-07-23 12:55:26 +0300

    build: update library configuration and include test files in exclusions

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,8 @@ export default defineConfig(() => ({
         'src/UI',
         'src/Swipi/hooks',
         'src/Swipi/helpers',
-        'src/Swipi/constants.ts',
-        'src/helpers'
+        'src/helpers',
+        'src/**/*.test.{ts,tsx}'
       ]
     })
   ],
@@ -26,7 +26,8 @@ export default defineConfig(() => ({
     lib: {
       entry: resolve('src', 'index.ts'),
       formats: ['es'],
-      fileName: 'index'
+      fileName: 'index',
+      cssFileName: 'style'
     },
     rollupOptions: {
       external: ['react']
